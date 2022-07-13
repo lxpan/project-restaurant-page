3c682856b084a2692bde019ec196831bfcbfc403
Luoxi Pan <l.panpax@gmail.com>
2022-07-13 18:22:46 +1000

    Installed HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
   entry: './src/index.js',
-  load: './src/page-load.js',
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
         {
-            test: /\.(png|svg|jpg|jpeg|gif|avif)$/i,
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
             type: 'asset/resource',
-        }
-    ]
-  }
+        },
+    ],
+  },
+  devtool: 'inline-source-map',
 };

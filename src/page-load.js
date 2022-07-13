08a965939477e37ed6d3edb1a732b20cd5051883
Luoxi Pan <l.panpax@gmail.com>
2022-07-13 17:46:30 +1000

    Recreate homepage using JS module

--- src/page-load.js
+++ src/page-load.js
@@ -1,4 +1,13 @@
 export default function pageLoad(doc) {
     const content = doc.querySelector('#content');
-    console.log(content);
+    const headingOne = doc.createElement('h1');
+    const headingTwo = doc.createElement('h2');
+    const image = doc.createElement('img');
+    const paragraph = doc.createElement('p');
+
+    headingOne.textContent = 'The Trevor James Appreciation Restaurant';
+    headingTwo.textContent = 'Tai Hao Le!';
+    paragraph.textContent = 'Hey guys check it out it\'s Trevor James! Let me dazzle you with my slurping and mastication skills!';
+
+    return content.append(headingOne, headingTwo, paragraph);
 }

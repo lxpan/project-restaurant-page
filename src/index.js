73cb887174566f93b2f453cf7bde36014067e322
Luoxi Pan <l.panpax@gmail.com>
2022-07-14 18:23:17 +1000

    Create content div using JS

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import pageLoad from "./page.js";
 
+// Create our skeleton #content div
+const contentDiv = document.createElement('div');
+contentDiv.id = 'content';
+document.body.appendChild(contentDiv);
+
 const content = pageLoad(document);
 console.log(__webpack_public_path__);
 console.log(content);

b2c85a64180e611138e4a9f1195eb21abbca3417
Luoxi Pan <l.panpax@gmail.com>
2022-07-14 18:53:22 +1000

    Set up tab group div for future content

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 import pageLoad from "./page.js";
 
+
+function createTabGroup() {
+    const tabGroup = document.createElement('div');
+    tabGroup.classList.add('tabs');
+
+    const buttonA = document.createElement('button');
+    const buttonB = document.createElement('button');
+    const buttonC = document.createElement('button');
+
+    buttonA.id = 'tabA';
+    buttonB.id = 'tabB';
+    buttonC.id = 'tabC';
+
+    buttonA.textContent = 'Button A';
+    buttonB.textContent = 'Button B';
+    buttonC.textContent = 'Button C';
+
+    tabGroup.append(buttonA, buttonB, buttonC);
+    
+    return tabGroup;
+}
+
+// Load tab group
+document.body.appendChild(createTabGroup());
+
 // Create our skeleton #content div
 const contentDiv = document.createElement('div');
 contentDiv.id = 'content';

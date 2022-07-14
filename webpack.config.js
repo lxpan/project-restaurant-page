01f842dfc667a81da7aea6e4205caed69bf3e569
Luoxi Pan <l.panpax@gmail.com>
2022-07-14 18:20:49 +1000

    Install and add HtmlWebpackPlugin to config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
     page: './src/page.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Trevor James Appreciation Restaurant',
+    }),
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),

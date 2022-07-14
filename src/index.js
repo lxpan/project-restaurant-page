6dade9f3c8f8923a9fee0fdcd3cfb3e4b5dd45b6
Luoxi Pan <l.panpax@gmail.com>
2022-07-14 19:53:58 +1000

    Create tab buttons and skeleton divs

--- src/index.js
+++ src/index.js
@@ -22,12 +22,29 @@ function createTabGroup() {
     return tabGroup;
 }
 
+function createTabContent(parent) {
+    const homeDiv = document.createElement('div');
+    const MenuDiv = document.createElement('div');
+    const contactDiv = document.createElement('div');
+
+    const tabDiv =[homeDiv, MenuDiv, contactDiv]
+    let counter = 1;
+    tabDiv.forEach(div => {
+        div.classList.add('tabContent');
+        // insert tab module's function here
+        div.textContent = `This is a tab content div for ${div.classList} ${counter++}!`;
+    });
+
+    parent.append(homeDiv, MenuDiv, contactDiv);
+}
+
 // Load tab group
 document.body.appendChild(createTabGroup());
 
 // Create our skeleton #content div
 const contentDiv = document.createElement('div');
 contentDiv.id = 'content';
+createTabContent(contentDiv);
 document.body.appendChild(contentDiv);
 
 // Load the placeholder homepage into #content div

ad30d6c2939a302744354e64db56e24ab6fe0b38
Luoxi Pan <l.panpax@gmail.com>
2022-07-20 16:43:19 +1000

    Remove unused 'hours' element

--- src/home.js
+++ src/home.js
@@ -6,7 +6,6 @@ export default function loadHome(parent) {
     const paragraph = document.createElement('p');
 
     const hoursHeading = document.createElement('h2');
-    const hoursPara = document.createElement('p');
     const hoursPre = document.createElement('pre');
 
     const locationHeading = document.createElement('h2');
@@ -21,7 +20,6 @@ export default function loadHome(parent) {
     paragraph.textContent = 'Located on the streets of Shinjuku, Toyko, in an unnamed alleyway, is your gateway to convenience. From salarymen burning the midnight oil to errant travellers, you can find everything that you need (and more) at Shinjuku 2049. We have the largest selection of beverages and snacks guaranteed to give you that buzz. We are Shinjuku 2049.';
     
     hoursHeading.textContent = 'Hours';
-    // hoursPara.textContent = 'Open 24/7, 7 days a week.'
     const hoursOpen = 'Open 24 hours';
     hoursPre.textContent = `Monday: ${hoursOpen}\nTuesday: ${hoursOpen}\nWednesday: ${hoursOpen}*\nThursday: ${hoursOpen}\nFriday: ${hoursOpen}\nSaturday: ${hoursOpen}\nSunday: ${hoursOpen}\n\n*Machines may occasionally be serviced at 12am midnight.`
     hoursPre.className = 'hoursOpen';
@@ -36,7 +34,6 @@ export default function loadHome(parent) {
         paragraph,
         homeImage,
         hoursHeading,
-        // hoursPara,
         hoursPre,
         locationHeading,
         locationPara

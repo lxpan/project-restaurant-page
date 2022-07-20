bf44ec52de5832553c9d9ab0be9ae7ec2a25b17d
Luoxi Pan <l.panpax@gmail.com>
2022-07-20 15:26:19 +1000

    Expand home page blurb

--- src/home.js
+++ src/home.js
@@ -11,7 +11,7 @@ export default function loadHome(parent) {
 
     headingOne.textContent = 'Shinjuku 2049 Vending Corp.';
     headingTwo.textContent = 'Bringing convenience back to the streets.';
-    paragraph.textContent = 'Welcome to Shinjuku 2049 Vending Corp! We have the largest selection of edibles and accessories.';
+    paragraph.textContent = 'Located on the streets of Shinjuku, Toyko, in an unnamed alleyway, is your gateway to convenience. From salarymen burning the midnight oil to errant travellers, you can find everything that you need (and more) at Shinjuku 2049. We have the largest selection of beverages and snacks guaranteed to give you that buzz. We are Shinjuku 2049.';
 
     parent.append(headingOne, headingTwo, paragraph, homeImage);
 }

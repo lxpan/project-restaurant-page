7a22d0ecb768966143cee7b7df667d69388fc7b8
Luoxi Pan <l.panpax@gmail.com>
2022-07-20 16:41:21 +1000

    Expand on hours open

--- src/home.js
+++ src/home.js
@@ -4,8 +4,11 @@ export default function loadHome(parent) {
     const headingOne = document.createElement('h1');
     const headingTwo = document.createElement('h2');
     const paragraph = document.createElement('p');
+
     const hoursHeading = document.createElement('h2');
     const hoursPara = document.createElement('p');
+    const hoursPre = document.createElement('pre');
+
     const locationHeading = document.createElement('h2');
     const locationPara = document.createElement('p');
     
@@ -18,7 +21,12 @@ export default function loadHome(parent) {
     paragraph.textContent = 'Located on the streets of Shinjuku, Toyko, in an unnamed alleyway, is your gateway to convenience. From salarymen burning the midnight oil to errant travellers, you can find everything that you need (and more) at Shinjuku 2049. We have the largest selection of beverages and snacks guaranteed to give you that buzz. We are Shinjuku 2049.';
     
     hoursHeading.textContent = 'Hours';
-    hoursPara.textContent = 'Open 24/7, 7 days a week.'
+    // hoursPara.textContent = 'Open 24/7, 7 days a week.'
+    const hoursOpen = 'Open 24 hours';
+    hoursPre.textContent = `Monday: ${hoursOpen}\nTuesday: ${hoursOpen}\nWednesday: ${hoursOpen}*\nThursday: ${hoursOpen}\nFriday: ${hoursOpen}\nSaturday: ${hoursOpen}\nSunday: ${hoursOpen}\n\n*Machines may occasionally be serviced at 12am midnight.`
+    hoursPre.className = 'hoursOpen';
+
+
     locationHeading.textContent = 'Location';
     locationPara.textContent = 'Located in Alleyway 2049, Shinjuku, Tokyo.'
 
@@ -28,7 +36,8 @@ export default function loadHome(parent) {
         paragraph,
         homeImage,
         hoursHeading,
-        hoursPara,
+        // hoursPara,
+        hoursPre,
         locationHeading,
         locationPara
         );

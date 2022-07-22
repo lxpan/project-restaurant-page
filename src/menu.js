e8002ecc20a3bd32751b11e97c3b13bb5f879a31
Luoxi Pan <l.panpax@gmail.com>
2022-07-22 15:46:57 +1000

    Create function to create menu items

--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,30 @@
+import drink from './images/menu/brawndo.webp';
+import foodPill from './images/menu/food-pills.webp';
+
 export default function loadMenu(parent) {
     const headingOne = document.createElement('h1');
 
+    
+    function createMenuItem(name, description, price, image) {
+        const menuItem = document.createElement('div');
+        menuItem.classList.add('menuItem');
+        const itemName = document.createElement('h3');
+        const itemDescription = document.createElement('p');
+        const itemPrice = document.createElement('span');
+
+        itemName.textContent = name;
+        itemDescription.textContent = description;
+        itemPrice.textContent = price;
+
+        const menuImage = new Image();
+        menuImage.src = image;
+        menuImage.className = 'menuImage';
+
+        menuItem.append(itemName, itemDescription, itemPrice, menuImage);
+
+        return menuItem;
+    }
+    
     headingOne.textContent = 'Menu';
 
     parent.append(headingOne);

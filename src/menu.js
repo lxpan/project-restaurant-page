a62164edbf07e916f2aba8bea736de55a4bd653b
Luoxi Pan <l.panpax@gmail.com>
2022-07-23 21:05:12 +1000

    Expand on descriptions for remaining menu items

--- src/menu.js
+++ src/menu.js
@@ -28,8 +28,8 @@ export default function loadMenu(parent) {
 
     const menuContainer = document.createElement('div');
     const menuDrink = createMenuItem('Super Energy Drink', 'An explosive energy drink infused with electrolytes, enzymes and over 100 stimulants. Start your day off with a bang!', '$2', drink);
-    const menuFoodPill = createMenuItem('Food Pill', 'It\'s got nutrition!', '$4', foodPill);
-    const menuBurger = createMenuItem('Burger-In-A-Can', "It's got (real) meat!", '$10', burger);
+    const menuFoodPill = createMenuItem('Food Pill', 'For the doers of the world, for those who live fast and die young. When there ain\'t enough time to eat, pop a Food Pill for an entire meal in one!', '$4', foodPill);
+    const menuBurger = createMenuItem('Burger-In-A-Can', "It's a burger! It's a can! No, it's a Burger-In-A-Can! Why settle for the real thing when you can settle for the next best thing!", '$2', burger);
 
     menuContainer.append(menuDrink, menuFoodPill, menuBurger);
     

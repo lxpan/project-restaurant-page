0a440607298921fdfc95cc747137d19294cbb96d
Luoxi Pan <l.panpax@gmail.com>
2022-07-25 10:30:15 +1000

    Add email to contact cards

--- src/contact.js
+++ src/contact.js
@@ -6,19 +6,21 @@ export default function loadContact(parent) {
     const headingOne = document.createElement('h1');
     headingOne.textContent = 'Contact';
 
-    function createContactItem(name, description, number, image) {
+    function createContactItem(name, description, number, email, image) {
         const contactItem = document.createElement('div');
         contactItem.classList.add('contactItem');
         
         const contactInfo = document.createElement('div');
         const contactName = document.createElement('h3');
         const contactDescription = document.createElement('p');
-        const contactNumber = document.createElement('span');
+        const contactNumber = document.createElement('p');
+        const contactEmail = document.createElement('p');
         
         contactName.textContent = name;
         contactDescription.textContent = description;
         contactNumber.textContent = number;
-        contactInfo.append(contactName, contactDescription, contactNumber);
+        contactEmail.textContent = email;
+        contactInfo.append(contactName, contactDescription, contactNumber, contactEmail);
 
         const contactImage = new Image();
         contactImage.src = image;
@@ -31,9 +33,9 @@ export default function loadContact(parent) {
 
     const contactContainer = document.createElement('div');
 
-    const robot = createContactItem('Robo', 'Robo tends to our vending machines.', '0400-555-2049', robo);
-    const gepetto = createContactItem('Gepetto', 'Our chief inventor.', '0400-555-2049', inventor);
-    const vendingMachine = createContactItem('Vendy', 'Our vendies provide impeccable service.', '0400-555-2049', vending);
+    const robot = createContactItem('Robo', 'Robo tends to our vending machines.', '0400-555-2049', 'notarealemail@2049.corp', robo);
+    const gepetto = createContactItem('Gepetto', 'Our chief inventor.', '0400-555-2049', 'notarealemail@2049.corp', inventor);
+    const vendingMachine = createContactItem('Vendy', 'Our vendies provide impeccable service.', '0400-555-2049', 'notarealemail@2049.corp.corp', vending);
 
     contactContainer.append(robot, gepetto, vendingMachine);
     parent.append(headingOne, contactContainer);
